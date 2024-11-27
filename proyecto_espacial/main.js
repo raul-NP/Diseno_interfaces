8183fb4d7cfe15f14060220846361730b72f8ed6
raul-NP <ranape4004@gmail.com>
2024-11-27 19:59:39 +0100

    a punto de terminar el proyecto espacial (falta palanca orbitar y sonidos)

--- proyecto_espacial/main.js
+++ proyecto_espacial/main.js
@@ -2,14 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let palanca = document.getElementById("palanca");
     let viaje = document.getElementById("fondo2");
+    let espacio2 = document.getElementById("fondo3");
 
-    palanca.addEventListener('click', () => {
+    palanca.addEventListener("click", () => {
+
+        //Reiniciamos la animación cada vez que pulsamos la palanca
+        viaje.style.animation = "none";
+        viaje.offsetWidth;        
+        viaje.style.animation = "";
         
-    })
+        //Cambiamos de capa a uno de los fondos
+        if (espacio2.style.zIndex == 2){
+            espacio2.style.zIndex = 12;
+        }else{
+            espacio2.style.zIndex = 2;
+        }
 
-    // palanca.addEventListener('mouseenter', () => {
-    //     palanca.setAttribute("src", "");
-    // })
+    });
+
+
 
 })
 
